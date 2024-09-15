382bfa20024d468bbffab7f1501a03f73c502aaf
KentseMatshira <kmatshira@yahoo.com>
2024-09-15 23:25:27 +0200

    updating java

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase , ref, push  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: https://console.firebase.google.com/project/realtime-database-be262/database/realtime-database-be262-default-rtdb/data/~2F
+  databaseURL:"https://console.firebase.google.com/project/realtime-database-be262/database/realtime-database-be262-default-rtdb/data/~2F" 
 };
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
@@ -23,6 +23,10 @@ function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+
+
 function appendItemToShoppingListEl(itemValue) {
     
     push(shoppingListInDB, inputValue)
@@ -31,5 +35,12 @@ function appendItemToShoppingListEl(itemValue) {
     console.log(inputValue)
      shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
-}
-
+function render(groceries) {
+    let listItems = ""
+    for (let i = 0; i < groceries.length; i++) {
+        listItems += `
+            <li>
+                    ${groceries[i]}
+            </li>
+        `
+    }
